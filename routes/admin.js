7dd4c3ba2c62cd5a553c78a067f5c1b016e55842
Király Bálint <kiraly.balint@simonyi.bme.hu>
2018-04-08 22:18:01 +0200

    Fix admin login

--- routes/admin.js
+++ routes/admin.js
@@ -11,10 +11,14 @@ router.get('/', function(req, res){
 });
 
 router.post('/login', adminCheck,  function(req, res){
+    res.redirect('/admin/events');
+});
+router.get('/events', adminCheck,  function(req, res){
     res.render('adminList');
-})
+});
+
 router.post('/createEvent', createEvent, listEvents, function(req, res){
     res.render('adminList');
-})
+});
 
 module.exports = router;
