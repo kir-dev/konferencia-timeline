91a3bf1d031971dbe308658eea0ee3179658914e
David Falusi <falusi.david94@gmail.com>
2018-04-15 23:19:51 +0200

    Add client side data polling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # konferencia-timeline
+
+## Deploy
+Before deploy, please add the api url to 'public/js/main.js'
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,94 @@
 $(document).ready(function () {
-    var socket = io();
-    Input.initialize(socket);
-
-    socket.on('events', function (message) {
-        $('#events).html(events(message));
-    });
-
+  module.polling();
 });
 
+const module = {
+  eventsHTML: '',
+  nextEvent: undefined,
+  currentEvent: undefined,
+  url: '',
 
 
-function events(events) {
-    var output = "";
-    events.forEach(function(event) {
-        output = output + "<tr><td><strong>"
-            + event.name + "</strong></td><td>"
-            + event.presenter + "</td><td>"
-            + event.startDate + "</td></tr>";
+  polling: function(){
+    $.get(module.url, function(data) {
+      module.eventsHTML = "";
+      module.nextEvent = undefined;
+      module.currentEvent = undefined;
+      module.fillEvents(data);
+      setTimeout(module.polling,5000);
     });
-    return output;
+  },
+
+  fillEvents: function(data) {
+    const events = data[0]['programs'];
+    events.forEach(module.processEvent);
+
+    if (module.currentEvent) {
+      const currentEvent = module.currentEvent;
+      var currentEventHTML =
+        `<h3>
+          <strong>${currentEvent.title}</strong> - ${currentEvent.performer}
+        </h3>
+        <span>${currentEvent.time}</span>
+        ${currentEvent.text}`;
+      $('.current-event').html(currentEventHTML);
+    } else {
+      $('.current-event').html("-");
+    }
+
+    if (module.nextEvent) {
+      const nextEvent = module.nextEvent;
+      var nextEventHTML =
+        `<h3>${nextEvent.title} - ${nextEvent.performer}</h3>
+        <span>${nextEvent.time}</span>`;
+      $('.next-event').html(nextEventHTML);
+    } else {
+      $('.next-event').html("-");
+    }
+
+    $('.events').html(module.eventsHTML);
+  },
+
+  processEvent: function(event) {
+    if (event.break == '1')
+      return;
+
+    const splittedTime = event.time.split('-');
+    const splittedStartTime = splittedTime[0].split(':');
+    const splittedendTime = splittedTime[1].split(':');
+
+    const startTime = new Date();
+    startTime.setHours(parseInt(splittedStartTime[0]));
+    startTime.setMinutes(parseInt(splittedStartTime[1]));
+
+    const endTime = new Date();
+    endTime.setHours(parseInt(splittedendTime[0]));
+    endTime.setMinutes(parseInt(splittedendTime[1]));
+
+    const currentDate = new Date();
+
+    var type;
+    if (endTime < currentDate) {
+      type = 'past';
+    }
+    else if (startTime > currentDate) {
+      type = 'next';
+      if (typeof module.nextEvent === 'undefined') {
+        module.nextEvent = event;
+      }
+    }
+    else {
+      module.currentEvent = event;
+      type = 'current'
+    };
+
+    module.eventsHTML +=
+      `<div class="card col-xs-12">
+        <div class="card-body ${type}">
+          <span>${event.time}</span>
+          <div class="separator"></div>
+          <strong>${event.title}</strong> - ${event.performer}
+        </div>
+      </div>`;
+  }
 }
